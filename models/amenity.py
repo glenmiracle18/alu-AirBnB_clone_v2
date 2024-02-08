b82f0e90ca5793550edbe96919debf077cd45030
glenmiracle18 <m.bonyu@alustudent.com>
2024-02-08 10:57:39 +0200

    user schema

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_t
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    doc
+    """
+    if storage_t == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity object"""
+        super().__init__(*args, **kwargs)
